ae34060d809801213f14e9b357b9ececaf167847
Milena Ivanova <milena_ii@abv.bg>
2017-08-14 12:09:22 +0300

    task - ListSumAndAverage, Reverse n integers with stack

---
+++ Lect_1/LinearDataStructures/1.CalcSumAver2/CalcSumAver2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.CalcSumAver2
+{
+    public class CalcSumAver2
+    {
+        public static void Main()
+        {
+            List<int> list = new List<int>();
+
+            string input = Console.ReadLine();
+            while (input != string.Empty)
+            {
+                int num = int.Parse(input);
+                list.Add(num);
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"sum is {list.Sum()}");
+            Console.WriteLine($"Average is {list.Average()}");
+
+        }
+    }
+}
---
+++ Lect_1/LinearDataStructures/Proba/Proba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba
+{
+    class Proba
+    {
+        static void Main()
+        {
+
+
+        }
+    }
+}
